983bb94ca7209c155320d453f7fe07d9ac194811
lzfgit-byte <1807409742@qq.com>
2023-04-02 15:07:42 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {doSomething} from "@/rootjs/mouse";
+import {doSomething} from "./rootjs/mouse.js";
 
 doSomething();
 console.log('446545656')
--- src/test.js
+++
@@ -1,4 +0,0 @@
-import {read} from "fs";
-import {readdir} from "fs";
-readdir('c:/',()=>{})
-console.log('1456465')
